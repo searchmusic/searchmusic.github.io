2db0bb86b2def3dbc18f2fb264d8bd8557cfdcc0
mattheweq <repos@mattheweq.com>
2025-03-04 10:05:28 +0000

    remove tofixed(2) on track / album duration

--- visuals/app.js
+++ visuals/app.js
@@ -45,10 +45,10 @@ const displayData = async (filterRange = null) => {
     <div class="track-item xl:w-[1280px] xl:h-[720px]">
       <p class="id"><span>[Catalogue ID]</span><span>&rarr;${item.id}</span></p>
       <p class="trackName"><span>TrackName</span><span class="title">${item.trackName}</span></p>
-      <p class="trackDuration"><span>Track Duration</span><span>${item.trackDuration.toFixed(2)}</span></p>
+      <p class="trackDuration"><span>Track Duration</span><span>${item.trackDuration}</span></p>
       <p class="trackNumber"><span>Sequence</span>#<span>${item.trackNumber}</span></p>
       <p class="albumName"><span>Album Title</span><span>${item.albumName}</span></p>
-      <p class="albumDuration"><span>Album Duration</span><span>${item.albumDuration.toFixed(2)}</span></p>
+      <p class="albumDuration"><span>Album Duration</span><span>${item.albumDuration}</span></p>
       <p class="releaseYear"><span>First Released:</span><span>${item.releaseYear}</span></p>
       <p class="genre"><span>Genre</span><span>${item.genre}</span></p>
 
